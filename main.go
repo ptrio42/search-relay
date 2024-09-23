f976fc40ff64fbb57161f3ff97357de2c6671312
ptrio42 <piotr@entereighth.com>
2024-09-23 15:03:19 +0200

    update

--- main.go
+++ main.go
@@ -1,42 +1,49 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/fiatjaf/eventstore/elasticsearch"
 	"github.com/fiatjaf/eventstore/lmdb"
 	"github.com/fiatjaf/khatru"
 	"github.com/joho/godotenv"
+	"github.com/nbd-wtf/go-nostr"
 	"log"
 	"net/http"
 	"os"
 )
 
 func main() {
-
 	godotenv.Load(".env")
 
 	relay := khatru.NewRelay()
 
 	relay.Info.Name = "Swarmstr Search Relay"
 	relay.Info.PubKey = "f1f9b0996d4ff1bf75e79e4cc8577c89eb633e68415c7faf74cf17a07bf80bd8"
-	//relay.Info.Description = "A relay accepting only GM notes!"
 	relay.Info.Icon = ""
 
-	db := &lmdb.LMDBBackend{Path: "./db"}
-	os.MkdirAll(db.Path, 0755)
+	db := lmdb.LMDBBackend{Path: "./db"}
 	if err := db.Init(); err != nil {
 		panic(err)
 	}
 
-	//search := bluge.BlugeBackend{Path: "./search", RawEventStore: db}
-	//if err := search.Init(); err != nil {
-	//	panic(err)
-	//}
+	search := &elasticsearch.ElasticsearchStorage{URL: getEnv("ES_URL")}
+	if err := search.Init(); err != nil {
+		panic(err)
+	}
 
-	search := elasticsearch.ElasticsearchStorage{URL: "http://elastic:" + getEnv("ELASTIC_PASSWORD") + "@es:9200", IndexName: "search"}
+	fmt.Println("Elasticsearch initialized successfully")
 
 	relay.StoreEvent = append(relay.StoreEvent, db.SaveEvent, search.SaveEvent)
-	relay.QueryEvents = append(relay.QueryEvents, db.QueryEvents, search.QueryEvents)
+	relay.QueryEvents = append(relay.QueryEvents, func(ctx context.Context, filter nostr.Filter) (chan *nostr.Event, error) {
+		if len(filter.Search) > 0 {
+			return search.QueryEvents(ctx, filter)
+		} else {
+			filterNoSearch := filter
+			filterNoSearch.Search = ""
+			return db.QueryEvents(ctx, filterNoSearch)
+		}
+	})
 	relay.DeleteEvent = append(relay.DeleteEvent, db.DeleteEvent, search.DeleteEvent)
 	relay.CountEvents = append(relay.CountEvents, db.CountEvents)
 
